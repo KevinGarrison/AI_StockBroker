bc34c327c4d9cfd51064bb882e42c5e45632c64e
Kevin Garrison <kevingarrison@outlook.de>
2025-03-24 11:15:47 +0100

    created .env.example, created vectorestore enrichment

--- backend/app.py
+++ backend/app.py
@@ -1,128 +1,237 @@
 import streamlit as st
 from utils.api_data_fetcher import API_Fetcher
 from utils.rag_chatbot import RAG_Chatbot
-import plotly.graph_objects as go
 from yahoo_fin import stock_info as si
-import numpy as np
 from dotenv import load_dotenv
+import pandas as pd
+from io import BytesIO
+from docling.document_converter import DocumentConverter
+from docling.datamodel.base_models import DocumentStream
+from qdrant_client import QdrantClient
+from langchain_qdrant import QdrantVectorStore
+from langchain_openai import OpenAIEmbeddings
+import re
+import os
 
 
 load_dotenv()
 
 fetcher = API_Fetcher()
 rag_chatbot = RAG_Chatbot()
-docs = None
-company_data={}
+    
 
-# Retrieve tickers for major indices
-dow_tickers = si.tickers_dow()
-nasdaq_tickers = si.tickers_nasdaq()
-sp500_tickers = si.tickers_sp500()
-
-all_tickers = set(dow_tickers + nasdaq_tickers + sp500_tickers)
-        
 st.title('Dev Chatbot')
 
-company_ticker = fetcher.fetch_company_cik_ticker_title()
+result = None
+    
+# Vars
+docs = None
+query=None
+ids_row = []
+company_data={}
+selected_id_title = {}
+filing_dfs_for_each_company = {}
+company_details_and_filing_acc = {}
+mapping_latest_forms_doc_index = {}
+important_forms = ['10-K', '10-Q', '8-K', 'S-1', 'S-3', 'DEF 14A', '20-F', '6-K', '4', '13D', '13G']
 
-common = set(all_tickers) & set(company_ticker["ticker"])
 
-# Step 2: Filter both
+# Get all Major Tickers and Fetch a Dataframe containing all CIK ID's, Ticker, Company Titles 
+# available on sec.gov and the get union of yFinance and SEC Data
+all_tickers = fetcher.fetch_all_major_indices()
+company_ids = fetcher.fetch_company_cik_ticker_title()
+common = set(all_tickers) & set(company_ids["ticker"])
 tickers = [t for t in all_tickers if t in common]
-company_ticker = company_ticker[company_ticker["ticker"].isin(common)]
+company_ticker = company_ids[company_ids["ticker"].isin(common)]
 
-# st.write(len(tickers))
-# st.write(len(company_ticker))
-
-# st.write(company_ticker)
-
+# Selectbox
 selected_companies = st.multiselect('Choose your prefered stocks:', company_ticker['title'])
 
-selected_id_title = {}
-tickers = []
 
+for selected in selected_companies:
+    ids_row.append(company_ids[company_ids['title'] == selected])
+    
+if ids_row:
+    filtered_comp_ids = pd.concat(ids_row, ignore_index=True)   
+
+# Create a Mapping Dictionary of CIK and Company Title 
 if selected_companies:
+    for row in company_ticker[company_ticker['title'].isin(selected_companies)][['title', 'ticker']].itertuples(index=False):
+        company = row.title
+        ticker = row.ticker
+        company_data[company] = fetcher.fetch_stock_data_yf(ticker=ticker)
     for company in selected_companies:
-        selected_id_title[company] = company_ticker.\
+        inner_list = selected_id_title[company] = []
+        inner_list.append(company_ticker.\
         loc[company_ticker['title'] == company, 'cik_str'].\
-        astype(str).values[0].zfill(10)
-        tickers.append(company_ticker.\
+        astype(str).values[0].zfill(10))
+        inner_list.append(company_ticker.\
         loc[company_ticker['title'] == company, 'ticker'].\
         astype(str).values[0])
 
-
-# st.write(tickers)
-docs = fetcher.fetch_filings_accessions(selected_id_title)
-company_data = fetcher.fetch_company_data(tickers)
-
-# st.write(tickers)
-# st.write(selected_id_title)
-
-# if docs:
-    # st.write(docs)
+# Fetch 3 Types of data of SEC with CIK ID
+# 1. Type of Company, Name, Ticker, Exchange, Fiscal year
+# 2. Insider transactions, Company Category e.g. Large Accelerated Filter, Adresses
+# 3. Filings
+# Stored in a Dictionary with Key (Company Name) Value (List[first,second,filings])
+for company, id_list in selected_id_title.items():
+    first, second, filings = fetcher.fetch_company_details_and_filing_accessions(id_list[0])
+    company_details_and_filing_acc[company] = [first, second, filings]
 
 
-# for tick in tickers:
-#     ticker = yf.Ticker(tick)
+# Create a Dictionary with Key (Company Name) and 
+# Value Dictionary Key (all available Forms of filings) Value (index where its stored)
+for company, first_second_filings in company_details_and_filing_acc.items():
+    mapping_latest_forms_doc_index[company] = {}
+    for form in important_forms:
+        for index, row in enumerate(first_second_filings[2]['recent']['form']):
+            if str(row) == form:
+                mapping_latest_forms_doc_index[company][form] = index
+                break
 
-#     # Market cap and PE ratio
-#     st.subheader(f"📊 {tick} Fundamentals")
-#     st.write("**Market Cap:**", ticker.info.get("marketCap"))
-#     st.write("**Trailing PE:**", ticker.info.get("trailingPE"))
+# Create the DataFrame with accession number of filings 
+for company, ids in selected_id_title.items():
+    cik = ids[0]
+    last_accession_numbers = []
+    report_dates = []
+    forms = []
+    primary_docs = []
+    idxs = []
 
-#     # Earnings calendar
-#     st.subheader("📅 Upcoming Earnings")
-#     st.write(ticker.calendar)
+    for form, index in mapping_latest_forms_doc_index[company].items():
+        last_accession_numbers.append(company_details_and_filing_acc[company][2]['recent']['accessionNumber'][index].replace('-', ''))
+        report_dates.append(company_details_and_filing_acc[company][2]['recent']['reportDate'][index])
+        forms.append(company_details_and_filing_acc[company][2]['recent']['form'][index])
+        primary_docs.append(company_details_and_filing_acc[company][2]['recent']['primaryDocument'][index])
+        idxs.append(index)
 
-#     # Dividends
-#     st.subheader("💸 Recent Dividends")
-#     st.write(ticker.dividends.tail(5))
+    df = pd.DataFrame({
+        'accession_number': last_accession_numbers,
+        'report_date': report_dates,
+        'form': forms,
+        'docs': primary_docs,
+        'cik': [cik] * len(forms),
+        'index': idxs
+    })
 
-#     # Price history
-#     st.subheader("📈 Price History (1 Month)")
-#     st.line_chart(ticker.history(period="1mo")["Close"])
-
-#     # News
-#     st.subheader("📰 Latest News")
-#     for article in ticker.news[:3]:
-#         st.write(f"**{article['content']['title']}**")
-#         st.write(article['content']['summary'])
-
-
-        
-    # st.write(company_data)
     
-if st.button('OpenAI4o-mini'):
-    for ticker, data in company_data.items():
-        ts = fetcher.fetch_stock_data_yf(ticker, '1y')
-        st.header(ts['name'] + f' ({ticker}):')
-        st.line_chart(data.get('price_history').dropna())
-        #st.write(info)
-        st.write(ts.get('info').get("longBusinessSummary"))
-        st.write()
-        st.write('Open:', ts.get('info').get('open'))
-        st.write('Low:', ts.get('info').get('dayLow'))
-        st.write('High:', ts.get('info').get('dayHigh'))
-        st.write('Volume:', ts.get('info').get('volume'))
-        st.write(ts['price_history'])
-        with st.spinner('AI Assitant analyzes data!'):
-            answer = rag_chatbot.gpt4o_mini(company_facts=ticker,context_y_finance=data, context_sec=None)
-            st.write(answer)
-if st.button('DeepseekR1'):
-    for (company, data), ticker in zip(company_data.items(), tickers):
-        ts = fetcher.fetch_stock_data_yf(ticker, '1y')
-        st.header(ts['name'] + f' ({ticker}):')
-        st.line_chart(data.get('price_history').dropna())
-        #st.write(info)
-        st.write(ts.get('info').get("longBusinessSummary"))
-        st.write()
-        st.write('Open:', ts.get('info').get('open'))
-        st.write('Low:', ts.get('info').get('dayLow'))
-        st.write('High:', ts.get('info').get('dayHigh'))
-        st.write('Volume:', ts.get('info').get('volume'))
-        st.write(ts['price_history'])
-        with st.spinner('AI Assitant analyzes data!'):
-            answer = rag_chatbot.deepseek_r1(company_facts=ticker, context_y_finance=data, context_sec=None)
-            st.write(answer)
+    filing_dfs_for_each_company[company] = df.copy()
+        
+    count = 0
+    # Fetch the identified filings from SEC.gov and store the results in a DataFrame for further processing
+    with st.spinner('Documents from SEC.gov (U.S. Securities and Exchange Commission) are getting  pulled and prepared...'):
+        for company, df in filing_dfs_for_each_company.items():
+            all_filings = []
+            for _, row in df.iterrows():
+                filings_dict = fetcher.fetch_company_filings(cik=row['cik'], accession=row['accession_number'], filename=row['docs'])
+                all_filings.append(filings_dict)
+            if all_filings:
+                st.success('Filings fetched!')
+            for index, content in enumerate(all_filings):
+                df.loc[index, 'content'] = content
+            for index, row in df.iterrows():
+                try:
+                    html_stream = DocumentStream(name=f'sec_{index}', stream=BytesIO(df.at[index, 'content']))
+                    converter = DocumentConverter()
+                    result = converter.convert(html_stream)
+                    df.loc[index, 'cleaned_docling_md'] = result.document.export_to_markdown()
+                except Exception:
+                    st.warning('Parsing Failed!')
+                    count +=1
+            st.write(count, '/', len(df), ' Docs Failed')
+            count = 0  
+            for index, row in df.iterrows():
+                content = row['cleaned_docling_md']
+                word_count = len(str(content).split()) if isinstance(content, str) else 0
+                df.loc[index, 'token_count_docling_cleaned_md'] = int(int(word_count) * 0.3)
+            df['Company'] = company
+            for index, row in df.iterrows():
+                if row.get('cleaned_docling_md'):
+                    rag_chatbot.process_text_to_qdrant(
+                        context_docs=str(row['cleaned_docling_md']),
+                        metadata={'Company': company,
+                                'Accession Number': row['accession_number'],
+                                'CIK': row['cik'],
+                                'Report Date':row['report_date'],
+                                'Form': row['form'],
+                                'Document': row['docs']}
+                        )
+
+# Load environment variables
+host = os.getenv('QDRANT_HOST')
+port = os.getenv('QDRANT_PORT')
+collection_name = os.getenv('COLLECTION_NAME')
+embedding_model = os.getenv('EMBEDDING_MODEL_OPENAI')
+
+# Build the Qdrant client URL
+url = f"http://{host}:{port}"
+
+# Create Qdrant client
+client = QdrantClient(host=host, port=int(port))  # Ensure port is int
+
+# Chat input
+#query = st.chat_input('Write a query to search in the SEC docs!')
+
+query = 'what is the biggest risc according to the sec filings in 2024?'
+
+if query:
+    embeddings = OpenAIEmbeddings(model=embedding_model)
+    
+    vector_store = QdrantVectorStore.from_existing_collection(
+        embedding=embeddings,
+        collection_name=collection_name,
+        url=url,
+    )
+    
+    result = vector_store.similarity_search(query=query, k=5)
+    rag_chatbot.print_results(result)
+    
+    combined_docs = []
+
+    for item in result:
+        doc = item[0] if isinstance(item, tuple) else item
+        content = doc.page_content
+        metadata = doc.metadata
+
+        # Turn metadata into a readable string
+        metadata_str = "\n".join(f"{key}: {value}" for key, value in metadata.items())
+
+        # Combine both
+        combined = f"Metadata SEC Files: {metadata_str}\n\nContent SEC:{content}"
+        combined_docs.append(combined)
+
+
+# if st.button('OpenAI4o-mini'):
+for ticker, data in company_data.items():
+    ts = data
+    st.header(ts['name'] + f' ({ticker}):')
+    st.line_chart(ts.get('price_history').dropna())
+    #st.write(info)
+    st.write(ts.get('info').get("longBusinessSummary"))
+    st.write()
+    st.write('Open:', ts.get('info').get('open'))
+    st.write('Low:', ts.get('info').get('dayLow'))
+    st.write('High:', ts.get('info').get('dayHigh'))
+    st.write('Volume:', ts.get('info').get('volume'))
+    st.write(ts['price_history'])
+    with st.spinner('AI Assitant analyzes data!'):
+        answer = rag_chatbot.gpt4o_mini(company_facts=ticker,context_y_finance=data, context_sec=combined_docs[0])
+        st.write(answer)
+# if st.button('DeepseekR1'):
+#     for (company, data), ticker in zip(company_data.items(), tickers):
+#         ts = data
+#         st.header(ts['name'] + f' ({ticker}):')
+#         st.line_chart(ts.get('price_history').dropna())
+#         #st.write(info)
+#         st.write(ts.get('info').get("longBusinessSummary"))
+#         st.write()
+#         st.write('Open:', ts.get('info').get('open'))
+#         st.write('Low:', ts.get('info').get('dayLow'))
+#         st.write('High:', ts.get('info').get('dayHigh'))
+#         st.write('Volume:', ts.get('info').get('volume'))
+#         st.write(ts['price_history'])
+#         with st.spinner('AI Assitant analyzes data!'):
+#             answer = rag_chatbot.deepseek_r1(company_facts=ticker, context_y_finance=data, context_sec=combined_docs[0])
+#             st.write(answer)
     
     
--- backend/utils/api_data_fetcher.py
+++ backend/utils/api_data_fetcher.py
@@ -5,97 +5,132 @@ import streamlit as st
 from dotenv import load_dotenv
 import os
 import yfinance as yf
+from yahoo_fin import stock_info as si
 from datetime import date
+import json
 
 load_dotenv()
 
 @dataclass
 class API_Fetcher:
-    def fetch_company_cik_ticker_title(self):
-        
-        headers = {'User-Agent': os.environ.get('USER_AGENT_SEC')}
-        company_tickers = requests.get(
-            "https://www.sec.gov/files/company_tickers.json",
-            headers=headers
-        )
-        company_data = pd.DataFrame(company_tickers.json()).T
-        return company_data
     
-    def fetch_stock_data_yf(self, ticker, period="10y", interval="1mo"):
-        if not ticker:
-            raise ValueError(f"Fund '{ticker}' not found.")
-        
-        yf_ticker = yf.Ticker(ticker)
-        hist = yf_ticker.history(period=period, interval=interval)
-        info = yf_ticker.info
-        return {
-            "ticker": ticker,
-            "name": info.get("longName", ticker),
-            "expense_ratio": info.get("expenseRatio"),
-            "price_history": hist.get("Close"),
-            "info":info
-        }
+    def fetch_all_major_indices(self):
+        # Retrieve tickers for major indices
+        dow_tickers = si.tickers_dow()
+        nasdaq_tickers = si.tickers_nasdaq()
+        sp500_tickers = si.tickers_sp500()
+        all_tickers = set(dow_tickers + nasdaq_tickers + sp500_tickers)
+        return all_tickers
     
-    def fetch_filings_accessions(self, companies:dict):
-        
-        docs = {}
-        headers = {'User-Agent': os.environ.get('USER_AGENT_SEC')}
-        for company, cik in companies.items():
+    
+    def fetch_company_cik_ticker_title(self)->pd.DataFrame:
+        try:
+            headers = {'User-Agent': os.environ.get('USER_AGENT_SEC')}
+            company_tickers = requests.get(
+                "https://www.sec.gov/files/company_tickers.json",
+                headers=headers
+            )
+            company_data = pd.DataFrame(company_tickers.json()).T
+            return company_data
+        except Exception as e:
+            print(f"Failed 'fetch_company_cik_ticker_title': {e}")    
+    
+    def fetch_stock_data_yf(self, ticker, period="10y", interval="1mo")->dict[str, any]:
+        try:
+            if not ticker:
+                raise ValueError(f"Fund '{ticker}' not found.")
+            
+            yf_ticker = yf.Ticker(ticker)
+            hist = yf_ticker.history(period=period, interval=interval)
+            info = yf_ticker.info
+            return {
+                "ticker": ticker,
+                "name": info.get("longName", ticker),
+                "expense_ratio": info.get("expenseRatio"),
+                "price_history": hist.get("Close"),
+                "info":info
+            }
+        except Exception as e:
+            print(f"Failed 'fetch_stock_data_yf': {e}")    
+    
+    def fetch_company_details_and_filing_accessions(self, cik)->tuple[dict[str, any], dict[str, any], dict[str, any]]:
+        try:
+            USER_AGENT_SEC = os.getenv('USER_AGENT_SEC')
+            headers = {'User-Agent': USER_AGENT_SEC}
             filingMetaData = requests.get(
                 f'https://data.sec.gov/submissions/CIK{cik}.json',
                 headers=headers
             )
             filing_dict = filingMetaData.json()
-            recent_filings = pd.DataFrame(filing_dict['filings']['recent'])
-            cols = ['accessionNumber', 'reportDate', 'form', 'cik']
+
+            important_keys = [
+                "name", "tickers", "exchanges", "sicDescription",
+                "description", "website", "fiscalYearEnd"
+            ]
+
+            secondary_keys = [
+                "stateOfIncorporation", "stateOfIncorporationDescription",
+                "insiderTransactionForOwnerExists", "insiderTransactionForIssuerExists",
+                "category", "addresses"
+            ]
             
-            important_forms = ['10-K', '10-Q', '8-K', 'S-1', 'S-3', 'DEF 14A', '20-F', '6-K', '4', '13D', '13G']
+
+            # Filter dictionaries
+            first_meta_data_dict = {k: (v if v else "N/A") for k, v in filing_dict.items() if k in important_keys}
+            secondary_meta_data_dict = {k: (v if v else "N/A") for k, v in filing_dict.items() if k in secondary_keys}
+            filings = filing_dict['filings']
             
-            docs[company] = {}
-            for form_type in important_forms:
-                filtered = recent_filings[recent_filings['core_type'] == form_type]
-                if not filtered.empty:
-                    filtered = filtered.copy()
-                    filtered['cik'] = cik
-                    filing = filtered[cols].reset_index(drop=True).head(1)
-                    
-                    docs[company][form_type] = filing
-        return docs
+            return first_meta_data_dict, secondary_meta_data_dict, filings
+        except Exception as e:
+                print(f"Failed 'fetch_company_details_and_filings_accesions': {e}")    
     
-    def fetch_company_data(self, tickers):
+    def fetch_company_filings(self, cik, accession, filename)->bytes:
+        try:
+            USER_AGENT_SEC = os.getenv('USER_AGENT_SEC')
+            headers = {'User-Agent': USER_AGENT_SEC}
+            url = f"https://www.sec.gov/Archives/edgar/data/{cik}/{accession}/{filename}"
+            response = requests.get(url, headers=headers)
+            if response.status_code == 200:
+                data = response.content
+            return data
+        except Exception as e:
+            print(f"Failed 'fetch_company_filings': {e}")    
+    
+    
+    # def fetch_company_data(self, tickers)->dict[str, any]:
         
-        company_data = {}
+    #     company_data = {}
 
-        for tick in tickers:
-            try:
-                ticker = yf.Ticker(tick)
-                info = ticker.info
-                earnings_calendar = ticker.calendar
-                for key, value in list(earnings_calendar.items())[:3]:
-                    if isinstance(value, list):
-                        earnings_calendar[key] = [v.isoformat() for v in value]
-                    elif isinstance(value, date):
-                        earnings_calendar[key] = value.isoformat()
-                    else:
-                        earnings_calendar[key] = value
-                dividends = ticker.dividends.tail(5)
-                price_history = ticker.history(period="1y")["Close"]
-                news = [
-                    {
-                        "title":article['content']['title'],
-                        "summary": article['content']['summary']
-                    }
-                    for article in ticker.news[:3]
-                ]
+    #     for tick in tickers:
+    #         try:
+    #             ticker = yf.Ticker(tick)
+    #             info = ticker.info
+    #             earnings_calendar = ticker.calendar
+    #             for key, value in list(earnings_calendar.items())[:3]:
+    #                 if isinstance(value, list):
+    #                     earnings_calendar[key] = [v.isoformat() for v in value]
+    #                 elif isinstance(value, date):
+    #                     earnings_calendar[key] = value.isoformat()
+    #                 else:
+    #                     earnings_calendar[key] = value
+    #             dividends = ticker.dividends.tail(5)
+    #             price_history = ticker.history(period="1y")["Close"]
+    #             news = [
+    #                 {
+    #                     "title":article['content']['title'],
+    #                     "summary": article['content']['summary']
+    #                 }
+    #                 for article in ticker.news[:3]
+    #             ]
 
-                company_data[tick] = {
-                    "market_cap": info.get("marketCap"),
-                    "trailing_pe": info.get("trailingPE"),
-                    "earnings_calendar": earnings_calendar,
-                    "recent_dividends": dividends,
-                    "price_history": price_history,
-                    "news": news,
-                }
-            except Exception as e:
-                print(f"⚠️ Error loading {tick}: {e}")
-        return company_data
+    #             company_data[tick] = {
+    #                 "market_cap": info.get("marketCap"),
+    #                 "trailing_pe": info.get("trailingPE"),
+    #                 "earnings_calendar": earnings_calendar,
+    #                 "recent_dividends": dividends,
+    #                 "price_history": price_history,
+    #                 "news": news,
+    #             }
+    #         except Exception as e:
+    #             print(f"⚠️ Error loading {tick}: {e}")
+    #     return company_data
