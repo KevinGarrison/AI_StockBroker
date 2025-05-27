a135100100a8c5cdb868b9413f20045067f7ba23
Kevin Garrison <kevingarrison@outlook.de>
2025-05-27 21:10:43 +0200

    added routing for forecasting in api-gateway microservice

--- backend/microservices/api_gateway/app.py
+++ backend/microservices/api_gateway/app.py
@@ -48,37 +48,8 @@ async def get_reference_files(accession: str, filename: str):
         response = await client.get(f"http://rag-chatbot:8002/reference-doc-from-analysis/{accession}/{filename}")     
         return response.json()
 
-# @app.get("/companies")
-# async def companies():
-#     async with httpx.AsyncClient() as client:
-#         response = await client.get("http://localhost:8001/companies", timeout=120.0)
-#         return response.json()
-
-# @app.get("/stock-history/{ticker}")
-# async def stock_history(ticker: str):
-#     async with httpx.AsyncClient() as client:
-#         response = await client.get(f"http://localhost:8001/stock-history/{ticker}", timeout=120.0)
-#         return response.json()
-
-# @app.get("/company-facts/{ticker}")
-# async def company_facts(ticker: str):
-#     async with httpx.AsyncClient() as client:
-#         response = await client.get(f"http://localhost:8001/yfinance-company-facts/{ticker}", timeout=120.0)
-#         return response.json()
-
-# @app.get("/stock-broker-analysis/{ticker}")
-# async def stock_broker_analysis(ticker: str):
-#     async with httpx.AsyncClient() as client:
-#         all_company_data = await client.get(f"http://localhost:8001/company-context/{ticker}", timeout=300.0)
-#         logger.info("[AI Result 1] %s", all_company_data)
-#         context_data = all_company_data.json()
-#         logger.info("[AI Result 2] %s", context_data)
-#         response = await client.post(f"http://localhost:8002/relevant-sec-files/{ticker}", json=context_data, timeout=120.0)
-#         logger.info("[AI Result 3] %s", response)
-#         return response.json()
-
-# @app.get("/reference-doc/{accession}/{filename}")
-# async def get_reference_files(accession: str, filename: str):
-#     async with httpx.AsyncClient() as client:
-#         response = await client.get(f"http://localhost:8002/reference-doc-from-analysis/{accession}/{filename}", timeout=120.0)
-#         return response.json()
+@app.get("/forecast/{ticker}")
+async def get_forecast(ticker:str):
+    async with httpx.AsyncClient() as client:
+        response = await client.get(f"http://forecasting:8003/forecast/{ticker}")     
+        return response.json()
