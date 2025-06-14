67d6b122ad5b55aba5abff6e2ffb1c1e90412389
Pardis Ebrahimi <pardisebrahimi93@gmail.com>
2025-06-14 21:17:52 +0200

    Update forecast-module

--- backend/microservices/forecasting/forecast_module.py
+++ backend/microservices/forecasting/forecast_module.py
@@ -11,10 +11,9 @@ def forecast_stock(ticker: str, df: pd.DataFrame) -> dict:
 
     # Step 2: Remove timezone info, so it's datetime64[ns] (naive)
     df['ds'] = df['ds'].dt.tz_localize(None)    
-    df['unique_id'] = ticker
-    print(df['ds'])
+    df['unique_id'] = df['unique_id'].astype(str)
+    df['y'] = df['y'].astype(float)
     # Further features
-    #df['recommendation'] = 1 if data['recommendation'] == 'buy' else 0
     df['recommendation'] = (df['recommendation'] == 'buy').astype(int)
 
     feature_columns = [
@@ -26,47 +25,50 @@ def forecast_stock(ticker: str, df: pd.DataFrame) -> dict:
     # Fill NaN values in feature columns with 0.0
     for col in feature_columns:
         if col in df.columns:
-            df[col] = df[col].fillna(0.0)
+            df[col] = df[col].astype(float).fillna(0.0)
 
     # Modell
     model = RNN(h=5, input_size=12, max_steps=100)
-    nf = NeuralForecast(models=[model], freq='M')
+    nf = NeuralForecast(models=[model], freq='ME')
     nf.fit(df=df)
 
     # Forecasting
     forecast = nf.predict()
-    current_price = df['y'].iloc[-1]
-    mean_forecast_price = forecast['RNN'].mean()
-    price_diff = mean_forecast_price - current_price
-    percent_change = (price_diff / current_price) * 100
 
-    recommendation = trading_strategy(percent_change)
+    return forecast
 
-    return {
-        'ticker': ticker,
-        'current_price': float(current_price),
-        'forecast_price': float(mean_forecast_price),
-        'percent_change': float(percent_change),
-        'recommendation': recommendation
-    }
- 
-def trading_strategy(percent_change: float) -> str:
-    """
-    Returns a trading strategy based on the predicted percentage price change.
- 
-    Parameters :
-        percent_change : The forecast percentage change in the share price
- 
-    Return :
-        A text recommendation for a suitable option strategy.
-    """
-   
-    if percent_change > 5:
-        return f"📈 BUY CALL: Expected increase of {percent_change:.2f}% - use possible opportunity!"
-    elif percent_change < -5:
-        return f"📉 BUY PUT: Expected decrease of {abs(percent_change):. 2f}% - hedge risk!"
-    elif -2 <= percent_change <= 2:
-        return f"➖ HOLD / SELL COVERED CALL: Movement below 2% - sideways market."
-    else:
-        return f"🔁 STRADDLE / SPREAD: Movement possible, direction unclear (±{percent_change:.2f}%)"
+
+    # current_price = df['y'].iloc[-1]
+    # mean_forecast_price = forecast['RNN'].mean()
+    # price_diff = mean_forecast_price - current_price
+    # percent_change = (price_diff / current_price) * 100
+
+    # recommendation = trading_strategy(percent_change)
+
+    #     return {
+    #         'ticker': ticker,
+    #         'current_price': float(current_price),
+    #         'forecast_price': float(mean_forecast_price),
+    #         'percent_change': float(percent_change),
+    #         'recommendation': recommendation
+    #     }
     
+    # def trading_strategy(percent_change: float) -> str:
+    #     """
+    #     Returns a trading strategy based on the predicted percentage price change.
+    
+    #     Parameters :
+    #         percent_change : The forecast percentage change in the share price
+    
+    #     Return :
+    #         A text recommendation for a suitable option strategy.
+    #     """
+    
+    #     if percent_change > 5:
+    #         return f"📈 BUY CALL: Expected increase of {percent_change:.2f}% - use possible opportunity!"
+    #     elif percent_change < -5:
+    #         return f"📉 BUY PUT: Expected decrease of {abs(percent_change):.2f}% - hedge risk!"
+    #     elif -2 <= percent_change <= 2:
+    #         return f"➖ HOLD / SELL COVERED CALL: Movement below 2% - sideways market."
+    #     else:
+    #         return f"🔁 STRADDLE / SPREAD: Movement possible, direction unclear (±{percent_change:.2f}%)"
