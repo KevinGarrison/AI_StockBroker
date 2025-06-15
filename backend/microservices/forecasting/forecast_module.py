f2931479ebb0a1d22e9b3a0bbb0c5bb0e0714af6
Pardis Ebrahimi <pardisebrahimi93@gmail.com>
2025-06-15 13:43:23 +0200

    fix a bug

--- backend/microservices/forecasting/forecast_module.py
+++ backend/microservices/forecasting/forecast_module.py
@@ -36,39 +36,3 @@ def forecast_stock(ticker: str, df: pd.DataFrame) -> dict:
     forecast = nf.predict()
 
     return forecast
-
-
-    # current_price = df['y'].iloc[-1]
-    # mean_forecast_price = forecast['RNN'].mean()
-    # price_diff = mean_forecast_price - current_price
-    # percent_change = (price_diff / current_price) * 100
-
-    # recommendation = trading_strategy(percent_change)
-
-    #     return {
-    #         'ticker': ticker,
-    #         'current_price': float(current_price),
-    #         'forecast_price': float(mean_forecast_price),
-    #         'percent_change': float(percent_change),
-    #         'recommendation': recommendation
-    #     }
-    
-    # def trading_strategy(percent_change: float) -> str:
-    #     """
-    #     Returns a trading strategy based on the predicted percentage price change.
-    
-    #     Parameters :
-    #         percent_change : The forecast percentage change in the share price
-    
-    #     Return :
-    #         A text recommendation for a suitable option strategy.
-    #     """
-    
-    #     if percent_change > 5:
-    #         return f"📈 BUY CALL: Expected increase of {percent_change:.2f}% - use possible opportunity!"
-    #     elif percent_change < -5:
-    #         return f"📉 BUY PUT: Expected decrease of {abs(percent_change):.2f}% - hedge risk!"
-    #     elif -2 <= percent_change <= 2:
-    #         return f"➖ HOLD / SELL COVERED CALL: Movement below 2% - sideways market."
-    #     else:
-    #         return f"🔁 STRADDLE / SPREAD: Movement possible, direction unclear (±{percent_change:.2f}%)"
