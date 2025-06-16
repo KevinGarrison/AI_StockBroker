8ea1f213f4b8dad24f1cc4f791a68f2d35a8f995
Ilef <75952867+ilefkalboussi@users.noreply.github.com>
2025-06-16 09:55:24 +0200

    include Response import and enhance Grafana dashboard endpoint

--- backend/microservices/Influx/inf_data_collector.py
+++ backend/microservices/Influx/inf_data_collector.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-
+from fastapi import FastAPI, Response
+import json
 from influx import DataCollector, InfluxDBHandler
 import asyncio
 
 app = FastAPI()
 
+# Endpoint to collect stock data and write to InfluxDB
 @app.get("/collect")
 async def collect_data(ticker: str):
     collector = DataCollector()
@@ -13,4 +15,13 @@ async def collect_data(ticker: str):
     influx_handler.write_stock_data(stock_data)
     influx_handler.close()
     #return {"status": "success", "ticker": ticker}
+
+
+
+# Endpoint to fetch Grafana dashboard JSON
+@app.get("/grafana-dashboard")
+def get_grafana_dashboard():
+    with open("Grafana.json", "r") as f:
+        dashboard_json = json.load(f)
+    return dashboard_json
 
