03092819120d3f25b85faaf4f45424ff3d4dc59f
Ilef <75952867+ilefkalboussi@users.noreply.github.com>
2025-06-18 16:05:55 +0200

    Logs from Docker into Influx

---
+++ extract_container_name.lua
@@ -0,0 +1,11 @@
+function extract_container_name(tag, timestamp, record)
+    local log_file = record["log"]
+    local container_id = record["container_id"]
+    if not container_id and record["filename"] then
+        container_id = string.match(record["filename"], "/var/lib/docker/containers/([a-f0-9]+)/")
+    end
+    if container_id then
+        record["container_id"] = container_id
+    end
+    return 1, timestamp, record
+end
